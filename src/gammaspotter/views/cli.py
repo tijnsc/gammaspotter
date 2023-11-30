48857f5ec1c2ad49b5b1b8f5db067581bf26a5ba
Tijn Schuitevoerder <tijnschuitevoerder@live.nl>
2023-11-30 14:12:39 +0100

    add plotting feature in cli.py

--- src/gammaspotter/views/cli.py
+++ src/gammaspotter/views/cli.py
@@ -1,4 +1,7 @@
 import click
+import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
 
 
 @click.group()
@@ -8,9 +11,25 @@ def cmd_group():
 
 
 @cmd_group.command()
-@click.argument("path", type=click.Path("rb"))
-def graph(path):
-    print(f"{path=}")
+@click.argument(
+    "path", type=click.Path("rb", dir_okay=False, executable=False, path_type=Path)
+)
+def graph(path: Path):
+    """Display a measurement in CSV format as an interactive MPL plot.
+
+    Args:
+        path (str): location of the data file that should be displayed
+    """
+    data = pd.read_csv(path)
+    data.plot(
+        "pulseheight",
+        "counts_ch_A",
+        legend=False,
+        xlabel="Pulseheight",
+        ylabel="Counts",
+        title=path.stem,
+    )
+    plt.show()
 
 
 if __name__ == "__main__":
