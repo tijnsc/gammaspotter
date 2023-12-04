91d984646a163bb6efd707f367b82e2c238386f1
Tijn Schuitevoerder <tijnschuitevoerder@live.nl>
2023-12-04 12:45:03 +0100

    add calibration flag in graph subcommand

--- src/gammaspotter/cli.py
+++ src/gammaspotter/cli.py
@@ -34,7 +34,23 @@ def cmd_group():
 @click.argument(
     "path", type=click.Path("rb", dir_okay=False, executable=False, path_type=Path)
 )
-def graph(path: click.Path, detect_peaks: bool, no_cleaning: bool, fit_peaks: bool):
+@click.option(
+    "-c",
+    "--calibrate",
+    type=click.Path(
+        exists=True,
+        dir_okay=False,
+        file_okay=True,
+        help="File containing calibration results which will be applied to the graph.",
+    ),
+)
+def graph(
+    path: click.Path,
+    detect_peaks: bool,
+    no_cleaning: bool,
+    fit_peaks: bool,
+    calibrate: bool,
+):
     """Display a measurement in CSV format as an interactive MPL plot.
 
     Args:
@@ -42,6 +58,7 @@ def graph(path: click.Path, detect_peaks: bool, no_cleaning: bool, fit_peaks: bo
         no_cleaning (bool): disable removal of edge effect
         detect_peaks (bool): indicate whether the peaks should be detected and displayed in the figure
         fit_peaks (bool): fit a gaussian function over the peaks to determine their positions more accurately
+        calibrate (bool): indicate a path where calibration results are stored
     """
     path = Path(path)
     data = pd.read_csv(path)
@@ -99,7 +116,7 @@ def graph(path: click.Path, detect_peaks: bool, no_cleaning: bool, fit_peaks: bo
 
 @cmd_group.command()
 @click.argument("path", type=click.Path())
-@click.argument("isotope", type=click.Choice(["Na-22", "test"]))
+@click.argument("isotope", type=click.Choice(["Na-22"]))
 @click.option(
     "-s",
     "--save",
@@ -128,11 +145,13 @@ def calibrate(path: click.Path, isotope: str, save: click.Path):
     if save:
         calibration_Series = pd.Series(
             {
-                "UNIX Timestamp": datetime.now(),
-                "Scaling factor": calibration_params[0],
-                "Horizontal offset": calibration_params[1],
+                "unix timestamp": datetime.now(),
+                "isotope": isotope,
+                "scaling factor": calibration_params[0],
+                "horizontal offset": calibration_params[1],
             }
         )
+
         save = Path(save)
         path = save / f"{isotope}_calibration.json"
         calibration_Series.to_json(path)
