ea51819dd48b87ca4c61c33071783d7068da220c
DylanTelleman <dylan.telleman@ziggo.nl>
2023-12-07 14:41:44 +0100

    Added the percentage of the sources to the lists

--- src/gammaspotter/match_features.py
+++ src/gammaspotter/match_features.py
@@ -12,18 +12,20 @@ def matcher(found_energy_sigma_list, energy_list):
             source_name = energy[0]
             energy_value = energy[1]
             sigma_source = abs(energy_value - found_energy) / sigma
-            output_list.append([peak_nr, source_name, sigma_source])
+            percentage = sigma_changer(sigma_source)
+            output_list.append([peak_nr, source_name, np.round(percentage, 2)])
         peak_nr += 1
+    return output_list
 
 
 def sigma_changer(sigma_source):
     x = sigma_source
-    percentage = erfc(x / np.sqrt(2))
+    percentage = erfc(x / np.sqrt(2)) *  100
     return percentage
 
 
 if __name__ == "__main__":
     list_1 = [[1475.5, 15], [511, 5]]
-    list_2 = [["Na-22", 1480], ["Cs-137", 500]]
-    matcher(list_1, list_2)
-    print(sigma_changer(1))
+    list_2 = [["Na-22", 1480], ["Cs-137", 510]]
+    print(matcher(list_1, list_2))
+
