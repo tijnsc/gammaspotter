a4b357e240c9f21dd1f4986f8040dcbb559936c0
DylanTelleman <dylan.telleman@ziggo.nl>
2023-12-07 16:25:46 +0100

    Made it so the list are sorted on percentage

--- src/gammaspotter/match_features.py
+++ src/gammaspotter/match_features.py
@@ -8,6 +8,7 @@ def matcher(found_energy_sigma_list, energy_list):
     peak_nr = 1
     number = 1
     count = 0
+    start = 0
     for found_energy_sigma in found_energy_sigma_list:
         found_energy = found_energy_sigma[0]
         sigma = found_energy_sigma[1]
@@ -16,23 +17,31 @@ def matcher(found_energy_sigma_list, energy_list):
             energy_value = energy[1]
             sigma_source = abs(energy_value - found_energy) / sigma
             percentage = sigma_changer(sigma_source)
-            output_list.append([peak_nr, source_name, np.round(percentage, 2)])
+            output_list.append([peak_nr, source_name, percentage])
         peak_nr += 1
 
+    
     for peak in output_list:
         if peak[0] == number:
             count += 1
-            sorted_output_list = sorted(output_list, reverse=True, key=lambda x: x[2])
         else:
-            count = 0
-            number += 1
-            cut_list = output_list[:count]
+            cut_list = output_list[start:count]
+            start = count
             sorted_cut_list = sorted(cut_list, reverse=True, key=lambda x: x[2])
             sorted_list = sorted_list + sorted_cut_list
+            number += 1
+            count += 1
+    cut_list = output_list[start:count]
+    sorted_cut_list = sorted(cut_list, reverse=True, key=lambda x: x[2])
+    sorted_list = sorted_list + sorted_cut_list
+    
+        
         
 
 
-    return sorted_output_list
+    
+    
+    return sorted_list
 
 
 def sigma_changer(sigma_source):
@@ -42,7 +51,7 @@ def sigma_changer(sigma_source):
 
 
 if __name__ == "__main__":
-    list_1 = [[1475.5, 15], [511, 5]]
+    list_1 = [[1475.5, 15], [511, 5], [410, 20]]
     list_2 = [["Na-22", 1480], ["Cs-137", 510]]
     print(matcher(list_1, list_2))
 
