74f886fe15d8af84153e0e5508e7a955d511f62c
Tijn Schuitevoerder <tijnschuitevoerder@live.nl>
2023-12-08 16:44:45 +0100

    Refactor fit_models.py to use numpy functions directly

--- src/gammaspotter/fit_models.py
+++ src/gammaspotter/fit_models.py
@@ -1,11 +1,12 @@
-import numpy as np
+from numpy import exp, pi, sqrt
 
 
 class FitModels:
     def gaussian(x, amp, cen, wid, startheight):
-        return (amp / (np.sqrt(2 * np.pi) * wid)) * np.exp(
+        """1-d gaussian: gaussian(x, amp, cen, wid)"""
+        return (amp / (sqrt(2 * pi) * wid)) * exp(
             -((x - cen) ** 2) / (2 * wid**2)
         ) + startheight
 
     def lorenzian(x, amp, cen, wid, startheight):
-        return (amp / np.pi) * (wid / ((x - cen) ** 2 + wid**2)) + startheight
+        return (amp / pi) * (wid / ((x - cen) ** 2 + wid**2)) + startheight
