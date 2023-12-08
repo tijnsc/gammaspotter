9cf86a9ca9ea9e411de42db4017e41c2e5c29eba
Tijn Schuitevoerder <tijnschuitevoerder@live.nl>
2023-12-08 14:38:13 +0100

    Add lorenzian function to fit_models.py

--- src/gammaspotter/fit_models.py
+++ src/gammaspotter/fit_models.py
@@ -6,3 +6,6 @@ class FitModels:
         return (amp / (np.sqrt(2 * np.pi) * wid)) * np.exp(
             -((x - cen) ** 2) / (2 * wid**2)
         ) + startheight
+
+    def lorenzian(x, amp, cen, wid, startheight):
+        return (amp / np.pi) * (wid / ((x - cen) ** 2 + wid**2)) + startheight
